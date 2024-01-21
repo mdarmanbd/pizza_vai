5ce15359ae50bb3eb93e1fdeeb07a309f1c66720
mdarmanbd <md.armantext@gmail.com>
2024-01-21 23:21:32 +0600

    start the new pizza project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+
+
+    <h1 class="text-3xl font-bold underline text-red-800 bg-green-400">
+    Hello world!
+  </h1>
+
+
+  </div>
+ 
+</template>
+
+<style scoped>
+
+</style>
